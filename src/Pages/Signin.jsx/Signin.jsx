034cdd86784754b85f1e2e9f945b9b4c0cd2e4a4
asraful-07 <asraful0508@gmail.com>
2025-08-12 19:24:30 +0600

    add singin page

--- src/Pages/Signin.jsx/Signin.jsx
+++ src/Pages/Signin.jsx/Signin.jsx
@@ -46,7 +46,7 @@ const Signin = () => {
   };
 
   return (
-    <div className="min-h-screen flex items-center justify-center bg-orange-200">
+    <div className="min-h-screen flex items-center justify-center bg-orange-100">
       <div className="container mx-auto px-4">
         <div className="flex justify-center">
           <div className="bg-white p-8 rounded-lg shadow-lg w-full max-w-md">
