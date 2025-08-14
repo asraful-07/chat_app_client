f7043307a9834572ca40653499fbe2d744922f95
asraful-07 <asraful0508@gmail.com>
2025-08-14 12:16:33 +0600

    add  useAxiosSecure

--- src/hooks/useAxiosSecure.jsx
+++ src/hooks/useAxiosSecure.jsx
@@ -9,3 +9,7 @@ const useAxiosSecure = () => {
 };
 
 export default useAxiosSecure;
+
+// https://chat-app-ngl4.onrender.com
+// https://talk-app-server.onrender.com
+// https://kotha-boli.onrender.com
